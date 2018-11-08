19ae98e2e1816e3bd318c72370811028ba74cdf3
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-11-08 20:39:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,10 +19,5 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
   
-  get "/multiply/:num1/:num2" do
-    @num1 = params[:num1]
-    @num2 = params[:num2]
-    sum = "#{@num1}".to_i * "#{@num2}".to_i
-    sum.to_s
-  end 
+ x
 end
